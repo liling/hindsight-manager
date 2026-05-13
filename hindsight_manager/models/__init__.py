e25a36bc807a675fa6a6c0bf800ee4950748c914
Ling Li <lilingv@gmail.com>
2026-05-14 00:14:53 +0800

    feat: add AuditLog model

--- hindsight_manager/models/__init__.py
+++ hindsight_manager/models/__init__.py
@@ -1,10 +1,12 @@
 from hindsight_manager.models.api_key import ApiKey
+from hindsight_manager.models.audit_log import AuditLog
 from hindsight_manager.models.tenant import Tenant, TenantStatus
 from hindsight_manager.models.tenant_member import MemberRole, TenantMember
 from hindsight_manager.models.user import AuthProvider, User, UserRole
 
 __all__ = [
     "ApiKey",
+    "AuditLog",
     "AuthProvider",
     "MemberRole",
     "Tenant",
